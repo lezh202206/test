6ad1c88b8f8e1ad69f4ef50254891f52a09d68cf
lezh <68684702+lezh202206@users.noreply.github.com>
2024-04-12 15:35:23 +0800

    add .

--- PR/main.go
+++ PR/main.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/cobra"
+	"os"
 	GitPR "test/PR/PR"
 )
 
 func main() {
+	var (
+		title string
+		audit string
+	)
+
+	var rootCmd = &cobra.Command{
+		Use:   "PR-GO",
+		Short: "A CLI app with parameters using Cobra",
+		Run: func(cmd *cobra.Command, args []string) {
+			run(title, audit)
+		},
+	}
+
+	rootCmd.Flags().StringVarP(&title, "title", "t", "", "Set PR title")
+	rootCmd.Flags().StringVarP(&audit, "audit", "a", "", "Set PR auditName")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run(title, audit string) {
 	//确保当前目录是一个Git仓库
 	repoPath := "/Users/lezh/Desktop/GO/src/miniblog"
 	MyGit := GitPR.NewMyGit(repoPath)
@@ -17,7 +42,7 @@ func main() {
 	MyGit.PushBranch(newBranch)
 
 	GiteePR := GitPR.NewGiteePR()
-	Url := GiteePR.SendGitee("test", branch, newBranch, "lzh123789")
+	Url := GiteePR.SendGitee(title, branch, newBranch, audit)
 	fmt.Println(Url)
 	//GiteePR.sendBot(fmt.Sprintf("PR: \n %s分支 合并到 %s分支 \n %s ", branch, newBranch, Url))
 }
