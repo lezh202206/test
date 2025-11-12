c69ce7f8668732ad02e4d0f1182c25d0ad160acb
lzh <68684702+lezh202206@users.noreply.github.com>
2025-11-12 16:10:43 +0800

    Refactor linked list with modular add/remove methods
    Reorganized linked list operations into modular methods for adding and removing nodes at the head, tail, and specific positions. Improved code readability and maintainability by separating concerns and adding index validation. Updated main function to use new method names and demonstrate usage.

--- camelCase/LinkedList/main.go
+++ camelCase/LinkedList/main.go
@@ -13,21 +13,12 @@ type LinkNodeList struct {
 	size int
 }
 
-func main() {
-	nodeList := &LinkNodeList{}
-	nodeList.Append(1)
-	nodeList.Append(2)
-	nodeList.Append(2)
-	nodeList.Append(3)
-	nodeList.DelAtPosition(1)
-	nodeList.Append(4)
-	nodeList.Append(5)
-	nodeList.Append(7)
-	nodeList.InsertAtPosition(5, 6)
-	nodeList.InsertAtPosition(7, 8)
-	nodeList.InsertAtPosition(7, 8)
-	nodeList.DelAtPosition(7)
-	nodeList.Println()
+func (l *LinkNodeList) IncSize() {
+	l.size++
+}
+
+func (l *LinkNodeList) DecSize() {
+	l.size--
 }
 
 func (l *LinkNodeList) Println() {
@@ -44,56 +35,147 @@ func (l *LinkNodeList) Println() {
 	}
 }
 
-// Append 每次在最后追加一个新链
-func (l *LinkNodeList) Append(v int) {
-	node := &Node{data: v, next: nil}
-	// 第一次 没有数据 类似初始化了
-	if l.size == 0 {
-		l.head = node
-	} else {
-		cur := l.head
-		// 如果这个链下面已经存在链的话就添加到下一个链中 以免被覆盖 应该是追加
-		// 这里要循环 找到最后一个
-		for cur.next != nil {
-			cur = cur.next
-		}
-		cur.next = node // 先把新链 填到末尾的 next 中
-	}
-	l.size++
+func (l *LinkNodeList) NewNode(v int) *Node {
+	return &Node{data: v, next: nil}
 }
 
-// InsertAtPosition 不能插入队首 不能超过链路总长
-func (l *LinkNodeList) InsertAtPosition(index, v int) {
-	if index < 0 || index > l.size {
-		fmt.Printf("数组越界")
-		return
-	}
-	newNode := &Node{data: v, next: nil}
-	// 找到目标位置的链
-	// 左边的 next 要链给新的链 没有就是 链首
-	// 新链的 next 要填入右边的链 没有就是 null
+func (l *LinkNodeList) isEmpty() bool {
+	return l.head == nil
+}
+
+// validaIndex 验证数组越界
+func (l *LinkNodeList) validaIndex(index int) bool {
+	return index < 0 || index > l.size
+}
+
+// FindNodeLink 找到目标左链
+func (l *LinkNodeList) FindNodeLink(index int) *Node {
 	cur := l.head
 	for i := 0; i < index-1; i++ {
 		cur = cur.next
 	}
-
-	// 如果先把左链的 next 填充新链 就找不到右链了 ！！！！
-	newNode.next = cur.next // 要先把右链先填充到 新链的 next 中
-	cur.next = newNode      // 把新链 放到 左链的 next 中
-	l.size++
+	return cur
 }
 
-// DelAtPosition 删除指定节点
-func (l *LinkNodeList) DelAtPosition(index int) {
-	if index < 0 || index > l.size-1 {
-		fmt.Printf("数组越界")
+// ================== 新增 ================== //
+
+// AppendEnd 尾部追加一个
+func (l *LinkNodeList) AppendEnd(v int) {
+	if l.isEmpty() {
+		l.head = l.NewNode(v)
+	} else {
+		//  A -> B -> C
+		//  D ->
+		//  遍历至最后一个链 把新链填入到 最后一个链的 next 中
+		// A -> B -> C -> D
+		cur := l.head
+		for cur.next != nil {
+			cur = cur.next
+		}
+		cur.next = l.NewNode(v)
+	}
+	l.IncSize()
+}
+
+// AppendTop 头部追加一个
+func (l *LinkNodeList) AppendTop(v int) {
+	if l.isEmpty() {
+		l.head = l.NewNode(v)
+	} else {
+		//  B -> C -> D
+		//  A -> (next)
+		//  A -> B -> C -> D
+		temp := l.NewNode(v)
+		temp.next = l.head
+		l.head = temp
+	}
+	l.IncSize()
+}
+
+// AddLocationNode 指定位置加一个
+func (l *LinkNodeList) AddLocationNode(index int, v int) {
+	if l.validaIndex(index) {
+		fmt.Println("数组越界")
+		return
+	}
+	if index == 0 {
+		l.AppendTop(v)
+	} else if index == l.size {
+		l.AppendEnd(v)
+	} else {
+		// 找到最终插入 位置
+		cur := l.FindNodeLink(index) // 左链
+
+		newNode := l.NewNode(v)
+		// 如果先填入左链 会导致右链全部丢失
+		newNode.next = cur.next // 先把右链 填入到 新链的 next 中
+		cur.next = newNode      // 再把新链 填入到 左链的 next 中
+		l.IncSize()
+	}
+}
+
+// ================== 删除 ================== //
+
+// DelNodeTop 删除第一个链
+func (l *LinkNodeList) DelNodeTop() {
+	if l.isEmpty() {
+		return
+	}
+	if l.size == 1 {
+		l.head = nil
+	} else {
+		l.head = l.head.next
+	}
+	l.DecSize()
+}
+
+// DelNodeEnd 删除最后一个链
+func (l *LinkNodeList) DelNodeEnd() {
+	if l.isEmpty() {
 		return
 	}
 	cur := l.head
-	for i := 0; i < index; i++ {
+	for i := 0; i <= l.size-2; i++ {
+		// A -> B -> C
+		// A -> B
+		if l.size-2 == i || l.size == 0 { // 找到倒数第二个链 置为空  或者 总链就剩下最后一个
+			cur.next = nil
+			continue
+		}
 		cur = cur.next
 	}
-	temp := cur.next.next // 将下级节点给临时变量 也就是右边的链
-	cur.next = temp       // 把右链直接给左链的 next
-	l.size--              // 数量减 1
+	l.DecSize()
+}
+
+// DelLocationNode 指定位置删一个
+func (l *LinkNodeList) DelLocationNode(index int) {
+	if l.validaIndex(index) {
+		fmt.Println("数组越界")
+		return
+	}
+	if index == 0 {
+		l.DelNodeTop()
+	} else if index == l.size {
+		l.DelNodeEnd()
+	} else {
+
+		cur := l.FindNodeLink(index) // 左链
+		cur.next = cur.next.next
+		l.DecSize()
+	}
+}
+
+func main() {
+	nodeList := &LinkNodeList{}
+	nodeList.AppendEnd(3)
+	nodeList.AppendEnd(4)
+	nodeList.AppendTop(2)
+	nodeList.AddLocationNode(3, 5)
+	nodeList.AddLocationNode(0, 0)
+	nodeList.AddLocationNode(1, 1)
+	nodeList.DelNodeTop()
+	nodeList.DelNodeEnd()
+	nodeList.DelLocationNode(1)
+
+	nodeList.Println()
 }
