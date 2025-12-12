fc251f46c331e63cfa26017858e01ab750df7285
lzh <68684702+lezh202206@users.noreply.github.com>
2025-12-12 14:28:09 +0800

    Refactor CountSort to return sorted array
    Modified the Sort function to return a new sorted array instead of sorting in place. Updated CountSort to print the returned sorted array. Improved clarity and correctness of the counting sort implementation.

--- camelCase/Find/CountSort/CountSort.go
+++ camelCase/Find/CountSort/CountSort.go
@@ -7,24 +7,34 @@ import (
 // CountSort 计数排序
 func CountSort() {
 	var arr = []int32{2, 4, 3, 0, 2, 3, 0, 3}
-
-	Sort(arr)
-	fmt.Println(arr)
+	fmt.Println(Sort(arr))
 }
 
-func Sort(arr []int32) {
-	var countArr = make([]int32, len(arr))
-	for _, v := range arr { // 元素出现几次
+func Sort(arr []int32) []int32 {
+	// 1. 找最大值
+	maxVal := arr[0]
+	for _, v := range arr {
+		if v > maxVal {
+			maxVal = v
+		}
+	}
+	// 2. 计数数组
+	countArr := make([]int32, maxVal+1)
+	for _, v := range arr { // 有多少个相同的元素
 		countArr[v]++
 	}
+
+	// 核心用前一个元素的值 +当前元素的值
+	// 就是 该元素第一次出现的下标位置
 	for i := 1; i < len(countArr); i++ {
 		countArr[i] += countArr[i-1]
 	}
 
-	var resultArr = make([]int32, len(arr))
-	for i := len(arr) - 1; i >= 0; i-- {
-		countArr[arr[i]] = countArr[arr[i]] - 1
-		resultArr[countArr[arr[i]]] = arr[i]
+	resultArr := make([]int32, len(arr))
+	for i := len(arr) - 1; i >= 0; i-- { // 这里直接使用映射关系就行了 没消耗一次坐标 该元素位置要 -1
+		v := arr[i]
+		countArr[v]--
+		resultArr[countArr[v]] = v
 	}
-	fmt.Println(resultArr)
+	return resultArr
 }
