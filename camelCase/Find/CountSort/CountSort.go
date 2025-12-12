4bf26603b56d1353edd402d3ecdce6025c5e2dc6
lzh <68684702+lezh202206@users.noreply.github.com>
2025-12-12 14:22:55 +0800

    Update CountSort.go

--- camelCase/Find/CountSort/CountSort.go
+++ camelCase/Find/CountSort/CountSort.go
@@ -1,22 +1,30 @@
 package CountSort
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // CountSort 计数排序
 func CountSort() {
-	var arr = []int32{49, 38, 65, 97, 76, 13, 27, 49}
+	var arr = []int32{2, 4, 3, 0, 2, 3, 0, 3}
 
 	Sort(arr)
 	fmt.Println(arr)
 }
 
 func Sort(arr []int32) {
-
-	/**
-	求最大的个数
-	*/
 	var countArr = make([]int32, len(arr))
-	for i, v := range arr {
-		countArr[i] = v
+	for _, v := range arr { // 元素出现几次
+		countArr[v]++
 	}
+	for i := 1; i < len(countArr); i++ {
+		countArr[i] += countArr[i-1]
+	}
+
+	var resultArr = make([]int32, len(arr))
+	for i := len(arr) - 1; i >= 0; i-- {
+		countArr[arr[i]] = countArr[arr[i]] - 1
+		resultArr[countArr[arr[i]]] = arr[i]
+	}
+	fmt.Println(resultArr)
 }
